e236f7cc762789a62482b64fc1a7f0cec0ca67fb
Tushar Gadhe <sudarshantushar04@gmail.com>
2026-06-08 13:24:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+API_ID = 1892985               # Apna API ID yahan dalein (bina quotes ke)
+API_HASH = "90896ccca5baa0af7f9f0219aac5eea0"     # Apna API Hash yahan dalein
+BOT_TOKEN = "8963963277:AAHpfu52CvsmcFFzUqwaqG6nWkJH_ONONYk"   # BotFather wala token
+
+# Jo lamba sa text abhi generate hua tha, use yahan paste karein
+SESSION_STRING = "BQAc4nkAbONr5EEeV4H9LmTCQlknsG1djs7beARMoTIw0-PaBEzJTicWVp83iSKYOutvBXPqNL6pjKG5mxXHYhUe6oYxNUliOY645rrLigONhguHXgGpXpaaLtKzIR3LpbPrmkbBncCZid4GzlfKzClzdDS2Lx1LH3MetvkpEiNA5kaWAjczJRt_k2jBnVcalLI1-M1PYkDziwZLUOAtIwnLwC2-2TiQODMkPBcI7UouPsiGCHNTQuLWibVOhMPhAsakGmINMPCZUkKP-ZMscXWTh_wGyFbDXmh3TuxGLxpEpP-E4GEEuod8Eu7EibLlY1yVmDJ8N4_SOOSNV6teEqBGWHCHAAAAAAIFLnG6AA"
+
+# Us Private Group ka ID jahan aapko AI alerts bhejega (-100 se shuru hota hai)
+# Agar abhi ID nahi pata, toh isko aise hi chhod dein, hum baad me nikal lenge.
+ADMIN_GROUP_ID = -1003745925732
